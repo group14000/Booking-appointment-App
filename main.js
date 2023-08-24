0cbb191490f994f9a83b2f3ca88d2d6203090672
DIGONTA HALDER <digantahalder14000@gmail.com>
2023-08-24 20:09:42 +0530

    second commit

--- main.js
+++ main.js
@@ -1,43 +1,58 @@
-// Wait for the DOM to be ready
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("my-form");
-  
+  const usersList = document.getElementById("users");
+
+  // Load existing data from localStorage
+  const storedData = JSON.parse(localStorage.getItem("userData")) || [];
+
+  // Display existing data on page load
+  storedData.forEach((user) => {
+    const li = document.createElement("li");
+    li.textContent = `${user.name} | ${user.email} | ${user.number}`;
+    usersList.appendChild(li);
+  });
+
   form.addEventListener("submit", function (e) {
-    e.preventDefault(); // Prevent the default form submission behavior
+    e.preventDefault();
 
     const nameInput = document.getElementById("name");
     const emailInput = document.getElementById("email");
+    const numberInput = document.getElementById("number");
 
     const name = nameInput.value;
     const email = emailInput.value;
+    const number = numberInput.value;
 
-    // Check if localStorage is available
-    if (typeof(Storage) !== "undefined") {
-      // Retrieve the existing data from localStorage
-      let userData = localStorage.getItem("userData");
-      
-      // If no existing data, initialize an empty array
-      if (!userData) {
-        userData = [];
-      } else {
-        userData = JSON.parse(userData);
-      }
+    if (name && email && number) {
+      const newUser = {
+        name: name,
+        email: email,
+        number: number,
+      };
 
-      // Add the new user data to the array
-      userData.push({ name, email });
+      storedData.push(newUser);
 
-      // Store the updated data in localStorage
-      localStorage.setItem("userData", JSON.stringify(userData));
+      // Save updated data to localStorage
+      localStorage.setItem("userData", JSON.stringify(storedData));
 
-      // Clear the form inputs
+      // Display the new user on the page
+      const li = document.createElement("li");
+      li.textContent = `${name} | ${email} | ${number}`;
+      usersList.appendChild(li);
+
+      // Clear input fields
       nameInput.value = "";
       emailInput.value = "";
+      numberInput.value = "";
 
-      // Provide a success message
+      // Show success message
       const msgDiv = document.querySelector(".msg");
-      msgDiv.textContent = "User data stored successfully.";
+      msgDiv.textContent = "User added successfully!";
+      setTimeout(() => {
+        msgDiv.textContent = "";
+      }, 2000);
     } else {
-      alert("localStorage is not supported in this browser.");
+      alert("Please fill in all the fields.");
     }
   });
 });
